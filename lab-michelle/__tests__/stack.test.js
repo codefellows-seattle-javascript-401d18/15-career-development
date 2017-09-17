d23df42a8f6705bf7896fa4b034c82114456af84
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-09-17 11:13:41 -0700

    Tests claim Stack isn't a constructor?

---
+++ lab-michelle/__tests__/stack.test.js
@@ -0,0 +1,16 @@
+'use strict';
+const Stack = ('../lib/stack');
+const superagent = require('superagent');
+require('jest');
+
+describe('Testing stack methods', function () {
+  describe('Testing push', () => {
+    test('should return 1', () => {
+      let stackOne = new Stack;
+      stackOne.push(3);
+      stackOne.push(2);
+      stackOne.push(1);
+      expect(stackOne.peek).toBe(1);
+    });
+  });
+});
---
+++ lab-michelle/lib/stack.js
@@ -0,0 +1,44 @@
+'use strict';
+const Node = require('./node');
+
+module.exports = class Stack {
+  constructor () {
+    this.top = null;
+    //note: could also have this.maxSize = max.Size || null
+    //Also could have: this.size = 0;
+  }
+
+  //Big O runtime: O(1)
+  ourPush(val) {
+    let node = new Node(val);
+
+    if (!this.top) {
+      this.top = node;
+      return this.top;
+    }
+
+    node.next = this.top;
+    this.top = node;
+    return this.top;
+  }
+
+  //Big O runtime: O(1)
+  ourPop(val) {
+    if (!this.top) return this.top = null;
+
+    let poppedOff = this.top;
+    this.top = this.top.next;
+    return poppedOff;
+  }
+
+  //Big O runtime: O(1)
+  ourPeek(val) {
+    if (!this.top) return null;
+    return this.top;
+  }
+
+  balancedBrackets() {
+
+  }
+  //reverse polish notation using your stack = bonus point;
+};
